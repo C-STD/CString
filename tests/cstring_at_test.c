9edd8725a0d0cee5f0ba786750a11defd17ee40a
Diego Vaccher <github@vaccherdiego.com>
2020-11-09 18:41:48 +0000

    add: `CStringAt` tests.
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_at_test.c
+++ tests/cstring_at_test.c
@@ -3,5 +3,18 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+
+	assert(*CStringAt(my_string, 0) == 'H');
+	assert(*CStringAt(my_string, 1) == 'E');
+	assert(*CStringAt(my_string, 2) == 'L');
+	assert(*CStringAt(my_string, 3) == 'L');
+	assert(*CStringAt(my_string, 4) == 'O');
+	assert(CStringAt(my_string, 52) == NULL);
+
+	CStringDestroy(my_string);
+
 	return 0;
 }
