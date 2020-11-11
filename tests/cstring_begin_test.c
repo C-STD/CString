7b87e64395029c0e86a196603a9bcef3d78b5545
Diego Vaccher <github@vaccherdiego.com>
2020-11-11 07:04:48 +0000

    add: test for `CStringBegin`.
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_begin_test.c
+++ tests/cstring_begin_test.c
@@ -3,5 +3,13 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+
+	assert(*(char*)(CStringBegin(my_string)) == 'H');
+
+	CStringDestroy(my_string);
+
 	return 0;
 }
