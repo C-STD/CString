ee73eca89ee024a98132d886ea64ece7d9589e72
Diego Vaccher <github@vaccherdiego.com>
2020-11-11 08:17:41 +0000

    add: test for `CStringData`.
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_data_test.c
+++ tests/cstring_data_test.c
@@ -3,5 +3,14 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+
+	assert(CStringData(my_string) != NULL);
+	assert(strcmp("HELLO", CStringData(my_string)) == 0);
+
+	CStringDestroy(my_string);
+
 	return 0;
 }
