19788ccaee9182fafdddba4bb44be02171908d11
Diego Vaccher <github@vaccherdiego.com>
2020-11-11 07:12:45 +0000

    add: test for `CStringCapacity`
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_capacity_test.c
+++ tests/cstring_capacity_test.c
@@ -3,5 +3,13 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+
+	assert(CStringCapacity(my_string) == (size_t)(9));
+
+	CStringDestroy(my_string);
+
 	return 0;
 }
