f983a1f70291415e3b689f36cddda52b7c398540
Diego Vaccher <github@vaccherdiego.com>
2020-11-12 15:48:50 +0000

    add: test for `CStringEqual`.
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_equal_test.c
+++ tests/cstring_equal_test.c
@@ -3,5 +3,14 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+	assert(strcmp(CStringData(my_string), "HELLO\0") == 0);
+	assert(CStringEqual(my_string, "LONGER TEST STRING\0") == 19);
+	assert(strcmp(CStringData(my_string), "LONGER TEST STRING\0") == 0);
+	
+	CStringDestroy(my_string);
+
 	return 0;
 }
