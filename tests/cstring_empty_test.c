3a5d0584285aa17b29914110d8448555c76dca2f
Diego Vaccher <github@vaccherdiego.com>
2020-11-12 15:41:25 +0000

    add: test for `CStringEmpty`
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_empty_test.c
+++ tests/cstring_empty_test.c
@@ -3,5 +3,12 @@
 #include <string.h>
 int main()
 {
+	void* my_string = CStringCreate();
+
+	assert(CStringEmpty(my_string) == 0);
+	assert(CStringEqual(my_string, "HELLO\0") == 6);
+	assert(CStringEmpty(my_string) != 0);
+
+	CStringDestroy(my_string);
 	return 0;
 }
