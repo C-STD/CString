afc527d1a41b436270082a32b71d1d590af001b6
Diego Vaccher <github@vaccherdiego.com>
2020-11-13 19:50:15 +0000

    update: test for `CStringErase`, remove comment.
    Signed-off-by: Diego Vaccher <github@vaccherdiego.com>

--- tests/cstring_erase_test.c
+++ tests/cstring_erase_test.c
@@ -11,13 +11,6 @@ int main()
 	assert(CStringErase(my_string, 4) == 0);
 	assert(strcmp(CStringData(my_string), "HELL\0") == 0);
 
-	// This test will always fail because at the moment, `CStringErase` just
-	// replace the character at the given index with a NULL character(\0).
-	// So, basically the result in this test case would be that the string is now
-	// `empty`, or in simpler terms the CString is now equal to "\0ELL" and
-	// because `strcmp` reads until it finds a `\0`, the two strings are treated as
-	// different.
-	// TODO: Fix `CStringErase` to allow erasing at the start/middle of the CString.
 	assert(CStringErase(my_string, 0) == 0);
 	assert(strcmp(CStringData(my_string), "ELL\0") == 0);
 
